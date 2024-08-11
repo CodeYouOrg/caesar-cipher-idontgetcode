77257aa5461c6702944cf705751633c643b721cd
Gayatri <gayatri.harriee@gmail.com>
2024-08-11 19:22:55 -0400

    Code of ceasar-cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,30 @@
 # add your code here
+
+import sys
+import string
+
+i = 0
+cipher_right_shift = 5
+string1 = 'Thanks'
+letter = string1[i]
+string1 = input("Enter your Text :")
+print(string1)
+
+plain_list = string.ascii_lowercase
+#print(plain_list)
+
+
+for i in range(len(string1)):
+    letter = string1[i]
+    position_of_letter = string1.rfind(letter)
+    position_of_letter = plain_list.rfind(letter)
+    position_of_letter = position_of_letter + cipher_right_shift
+    print(plain_list[position_of_letter], end='')
+    
+    
+  
+
+
+    
+
+    
