e6426a37647305e0553f34c7917e19d6a69bbbc8
Gayatri <gayatri.harriee@gmail.com>
2024-08-18 17:19:00 -0400

    code for Caesar cipher

--- cipher.py
+++ cipher.py
@@ -9,7 +9,6 @@ cipher_right_shift = 5
 #letter = string1[i]
 string1 = input("Enter your Text :")
 print(string1)
-
 plain_list = string.ascii_lowercase
 #print(plain_list)
 
@@ -17,11 +16,11 @@ plain_list = string.ascii_lowercase
 for i in range(len(string1)):
     letter = string1[i]
     position_of_letter = string1.rfind(letter)
-    position_of_letter = plain_list.rfind(letter)
-    position_of_letter = position_of_letter + cipher_right_shift
+    position_of_plainletter = plain_list.rfind(letter)
+    position_of_letter = (position_of_plainletter + cipher_right_shift)%26
     print(plain_list[position_of_letter], end='')
 
-# need to figure out how to loop to the from of the string. 
+
     
     
   
